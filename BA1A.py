fe2c863f6284f52fd012cd6f3d32ab13ac051bac
tpavic1 <101116208+tpavic1@users.noreply.github.com>
2022-03-15 22:58:15 +0100

    Create BA1A.py

---
+++ BA1A.py
@@ -0,0 +1,14 @@
+def PatternCount(text,pattern):
+  count=0
+  for i in range(len(text)-len(pattern)+1):
+    if(text[i:i+len(pattern)]==pattern):
+      count+=1
+  return count
+
+sample_=''
+sample=sample_.splitlines()
+
+text=sample[0]
+pattern=sample[1]
+
+print(PatternCount(text,pattern))
