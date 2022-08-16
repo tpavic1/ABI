7b0086168419a5001afa89e69cf76189c4586c9f
tpavic1 <101116208+tpavic1@users.noreply.github.com>
2022-08-16 23:44:58 +0200

    Update BA2C.py

--- chapter2/BA2C.py
+++ chapter2/BA2C.py
@@ -1,35 +1,26 @@
-def Kmers(text,k):
-  kmers=[]
-  for i in range(len(text)-k+1):
-    kmer=text[i:i+k]
-    if (kmer not in kmers):
-      kmers.append(kmer)
-  return kmers
-
-
-def ProfileKmer(text,k,profile):
-  probabilities={}
-  for kmer in Kmers(text,k):
-    prob=1
+def ProfileProbability(profile, kmer, k):
+    probability=1
     for i in range(k):
-      if (kmer[i]=="A"):
-        prob=prob*profile[0][i]
-      elif (kmer[i]=="C"):
-        prob=prob*profile[1][i]
-      elif (kmer[i]=="G"):
-        prob=prob*profile[2][i]
-      else:
-        prob=prob*profile[3][i]
-    
-    probabilities[kmer]=prob
+        if kmer[i]=="A":
+            probability*=profile[0][i]
+        elif kmer[i]=="C":
+            probability*=profile[1][i]
+        elif kmer[i]=="G":
+            probability*=profile[2][i]
+        else:
+            probability*=profile[3][i]
+    return probability
 
-  max_val=max(probabilities.values())
-  for k in probabilities:
-    if (probabilities[k]==max_val):
-      max_kmer=k
-      break
-
-  return max_kmer
+def ProfileMostProbableKmer(text, profile, k):
+    probability=0
+    mostProbableKmer=text[:k]
+    for i in range(len(text)-k+1):
+        kmer=text[i:i+k]
+        probability_=ProfileProbability(profile, kmer, k)
+        if probability_>probability:
+            probability=probability_
+            mostProbableKmer=kmer
+    return mostProbableKmer
 
 ### ispis
 
@@ -40,11 +31,11 @@ k=int(sample[1])
 matrica=sample[2:]
 profile=[]
 for i in range(len(matrica)):
-  matrica[i]=matrica[i].split(" ")
-  row=[]
-  for j in range(len(matrica[i])):
-    row.append(float(matrica[i][j]))
-  profile.append(row)
+    matrica[i]=matrica[i].split(" ")
+    row=[]
+    for j in range(len(matrica[i])):
+        row.append(float(matrica[i][j]))
+    profile.append(row)
 
-print(ProfileKmer(text,k,profile))
+print(ProfileMostProbableKmer(text, profile, k))
 
