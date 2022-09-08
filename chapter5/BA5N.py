b026e6c8e5c801aab3eaa6894b96aa8efdb5346e
tpavic1 <101116208+tpavic1@users.noreply.github.com>
2022-09-08 03:23:49 +0200

    Create BA5N.py

---
+++ chapter5/BA5N.py
@@ -0,0 +1,53 @@
+def HasIncomingEdges(node, graph):
+    for key in graph:
+        if node in graph[key]:
+            return True
+    return False
+
+def HasEdges(graph):
+    for key in graph:
+        if(len(graph[key])>0):
+            return True
+    return False
+
+def TopologicalOrdering(graph, candidates):
+    lista=[]
+
+    while candidates:
+        a=candidates[0]
+        lista.append(a)
+        candidates.remove(a)
+        if a in graph:
+            for b in graph[a].copy():
+                graph[a].remove(b)
+                if not HasIncomingEdges(b, graph):
+                    candidates.append(b)
+
+    if HasEdges(graph):
+        return "the input graph is not a DAG"
+    else:
+        return ", ".join(lista)
+
+### ispis
+
+sample_=''''''
+sample=sample_.splitlines()
+
+graph={}
+allNodes=set()
+for line in sample:
+    nodes=line.split(" -> ")
+    node=nodes[0]
+    connections=nodes[1].split(",")
+    graph[node]=connections
+
+outgoing = set()
+for v in graph.values():
+    outgoing.update(v)
+
+candidates=[]
+for k in graph.keys():
+    if k not in outgoing:
+        candidates.append(k)
+
+print(TopologicalOrdering(graph, candidates))
